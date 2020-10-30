8370fe1363d786f6409445472a047d989d17c206
Dan Büschlen <danbueschlen@gmail.com>
2020-10-30 20:11:39 +0100

    Revert "get rid of sound"
    This reverts commit d57327a10a5262ec937b55e703ff5b6dbb487bf5.

--- src/sketches/endlessTunnel.js
+++ src/sketches/endlessTunnel.js
@@ -1,5 +1,6 @@
 import p5 from "p5"
 
+// import "../lib/p5.sound"
 import "../lib/p5.gui"
 
 const easeInQuad = pos => Math.pow(pos, 2)
@@ -54,6 +55,12 @@ const sketch = p => {
   window.p5 = p
 
   let canvas
+  let sound
+
+  p.preload = function() {
+    // p.soundFormats("mp3")
+    // sound = p.loadSound(require("../sound/Sanctuary.mp3"), soundLoaded)
+  }
 
   p.setup = function() {
     canvas = p.createCanvas(p.windowWidth, p.windowHeight)
@@ -149,6 +156,10 @@ const sketch = p => {
     if (inputMode) {
       p.background(inputModeOverlayColor)
     }
+
+    if (sound) {
+      sound.rate(p.map(windSpeed, windSpeedMin, windSpeedMax, 0.8, 1.8))
+    }
   }
 
   p.mouseClicked = (e) => {
@@ -161,6 +172,10 @@ const sketch = p => {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
     ox = p.width / 2
     oy = p.height / 2
+  }
+
+  function soundLoaded(sound) {
+    createAudioToggle(sound)
   }
 }
 
@@ -229,4 +244,30 @@ function fetchWeatherData(q) {
       loadingEnd()
       inputError()
     })
+}
+
+const loadingStart = () => {inputElement.classList.add("loading"); inputElement.disabled = true}
+const loadingEnd = () => {inputElement.classList.remove("loading"); inputElement.disabled = false}
+const inputError = () => inputElement.classList.add("error")
+const inputErrorReset = () => inputElement.classList.remove("error")
+
+
+function createAudioToggle(sound) {
+  const button = document.createElement("button")
+  button.classList.add("audio-toggle")
+
+  button.textContent = "⏯"
+  document.body.appendChild(button)
+
+  button.addEventListener("click", e => {
+    e.stopPropagation()
+    if (sound.isLooping()) {
+      sound.pause()
+    } else {
+      sound.loop()
+    }
+  })
+
+  sound.loop()
+  sound.setVolume(0.7)
 }
